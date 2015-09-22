7664b8c0ac598f4256a0059bda7d0b5c98188e48
Samantha Panganiban <samantha.isabellechivon@gmail.com>
2015-09-22 13:17:02 +1200

    add layout to nav bar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,24 +4,28 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title></title>
-    <link rel="stylesheet" href="css/foundation.css" />
-    <script src="js/vendor/modernizr.js"></script>
+    <link rel="stylesheet" href="/css/foundation.css" />
+    <script src="/js/vendor/modernizr.js"></script>
   </head>
   <body>
-
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/pokedex">Pokédex</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/faq">FAQ</a></li>
-        <li><a href="/auth/register">Register</a></li>
-        <li><a href="/auth/login">Login</a></li>
-        <li><a href="/pokecentre">Pokécentre</a></li>
-        <li><a href="/auth/logout">Logout</a></li>
-      </ul>
-    </nav>
+  
+  <div class="row">
+    <div class="columns">
+      <nav>
+        <ul class="inline-list">
+          <li><a href="/">Home</a></li>
+          <li><a href="/pokedex">Pokédex</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/faq">FAQ</a></li>
+          <li><a href="/auth/register">Register</a></li>
+          <li><a href="/auth/login">Login</a></li>
+          <li><a href="/pokecentre">Pokécentre</a></li>
+          <li><a href="/auth/logout">Logout</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 
     @yield('content')
     
