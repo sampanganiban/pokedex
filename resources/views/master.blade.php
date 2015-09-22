0ab33421d4bf4c178cf2fb3f88d5abe55a3624f5
Samantha Panganiban <samantha.isabellechivon@gmail.com>
2015-09-22 13:05:02 +1200

    add a nav bar for the site

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,6 +9,20 @@
   </head>
   <body>
 
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/pokedex">Pokédex</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/faq">FAQ</a></li>
+        <li><a href="/auth/register">Register</a></li>
+        <li><a href="/auth/login">Login</a></li>
+        <li><a href="/pokecentre">Pokécentre</a></li>
+        <li><a href="/auth/logout">Logout</a></li>
+      </ul>
+    </nav>
+
     @yield('content')
     
     <script src="js/vendor/jquery.js"></script>
